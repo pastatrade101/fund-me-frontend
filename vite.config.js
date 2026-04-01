fb3f35ef04f590f480590c9e801a3ebe9e1fd266
Pastory joseph <pastory56@gmail.com>
2026-04-01 15:26:36 +0300

    docker resolve

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,23 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 export default defineConfig({
     plugins: [react()],
+    optimizeDeps: {
+        include: [
+            "react",
+            "react-dom",
+            "@mui/material",
+            "@mui/icons-material",
+            "@emotion/react",
+            "@emotion/styled"
+        ]
+    },
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: undefined
+            }
+        }
+    },
     server: {
         port: 5174
     }
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,23 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
+    optimizeDeps: {
+        include: [
+            "react",
+            "react-dom",
+            "@mui/material",
+            "@mui/icons-material",
+            "@emotion/react",
+            "@emotion/styled"
+        ]
+    },
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: undefined
+            }
+        }
+    },
     server: {
         port: 5174
     }
